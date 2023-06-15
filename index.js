5e67571cdbbd2fd3eb6a29cc272b45bcffae8124
Divyanshb30 <134592525+Divyanshb30@users.noreply.github.com>
2023-06-15 12:16:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomDice = "images/dice" + randomNumber1 + ".png";
-document.querySelector(".img1").setAttribute("src", randomDice);
+var randomDice1 = "images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src", randomDice1);
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomDice = "images/dice" + randomNumber2 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", randomDice);
+var randomDice2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomDice2);
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 1 Wins";
 }
